d67c17c5eaa2a98c15f1e86973e756db21c3c8f3
AlbertoArconada <alberto.arconada@gmail.com>
2022-07-12 18:32:52 +0200

    Execise 1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -15,25 +15,25 @@ const Footer = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
+  const parts = [{
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
-  }
+  }]
   return (
     <div>
       <Header course={course} />
-      <Part part={part1} />
-      <Part part={part2} />
-      <Part part={part3} />
-      <Footer exercises={part1.exercises + part2.exercises + part3.exercises}/>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+      <Footer exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   );
 };
