9027fa29e573cd9358e4866811cebe1a50ea7453
AlbertoArconada <alberto.arconada@gmail.com>
2022-07-12 18:28:03 +0200

    Execise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,34 +6,34 @@ const Header = (props) => {
 };
 
 const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
+  return <p>{props.part.name} {props.part.exercises}</p>
 }
 
 const Footer = (props) => {
-  return (
-    <p>Number of exercises {props.exercises}</p>
-  )
+  return <p>Number of exercises {props.exercises}</p>
 }
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header course={course} />
-      <Part name={part1} exercises={exercises1} />
-      <Part name={part2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
-      <Footer exercises={exercises1 + exercises2 + exercises3}/>
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
+      <Footer exercises={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   );
 };
